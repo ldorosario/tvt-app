1de5b743145d3a9c74203198b846446d00f0199c
Lucas do Rosário Pereira <ldorosario@users.noreply.github.com>
2024-03-17 22:37:37 -0300

    Update Membros.ts

--- src/dados/Membros.ts
+++ src/dados/Membros.ts
@@ -20,13 +20,70 @@ export const membrosEstaticos =
     } 
      
     },
-    {
-        nome:"Nome Teste2",
-         dados:{
-          telefone:"0002",
-          tipoSangue:"1-",
-        }
-        
-        },
+
+      {
+    nome:"Eduardo do Nascimento",
+     dados:{
+      dataEntradaEquipe:"16/03/2024",
+      dataNascimento:"14/10/1997",
+      rg:"5111522362",
+      cpf:"04356666062",
+      telefone:"51 998661629",
+      email:"eduardonascimentod97@gmail.com",
+      endereco:"Rua Brasil 782 Eldorado so sul",
+      tipoSangue:"Sem info",
+      alergias:"Nega",
+      hospitalPreferencia:"HPS",
+      contatosEmergencia: "51 998574849 - Pedro",
+      medicamentoUsoCont:"Nega",
+      problemaSaude: "Nega",
+      planoSaude: "Unimed(Empresa)"
+    } 
+     
+    },
+    
+      {
+    nome:"Íbero Pinto Da Silva",
+     dados:{
+      dataEntradaEquipe:"29/10/2022",
+      dataNascimento:"26/01/1979",
+      rg:"7073445277",
+      cpf:"94245231068",
+      telefone:"51 999917584",
+      email:"ibero.silva@gmail.com",
+      endereco:"Rua Pedro Alvares Cabral, 348. Gravatai-RS CEP: 94150-400",
+      tipoSangue:"O+",
+      alergias:"Nega",
+      hospitalPreferencia:"Sem Preferência",
+      contatosEmergencia: "51 996790447 - Milena(esposa)",
+      medicamentoUsoCont:"Nega",
+      problemaSaude: "Nega",
+      planoSaude: "Unimed"
+    } 
+     
+    },
+
+         {
+    nome:"Guilherme Grazzioli Moscon",
+     dados:{
+      dataEntradaEquipe:"Sem info",
+      dataNascimento:"28/05/1992",
+      rg:"9103981801",
+      cpf:"03103007007",
+      telefone:"51 999873417",
+      email:"grazzioli.guilherme@gmail.com",
+      endereco:"Major Sezefredo 1107, Canoas",
+      tipoSangue:"B+",
+      alergias:"Nega",
+      hospitalPreferencia:"Canoas",
+      contatosEmergencia: "51 991784485",
+      medicamentoUsoCont:"Nega",
+      problemaSaude: "Ligamento cruzado joelho esquerdo rompido",
+      planoSaude: "Unimed"
+    } 
+     
+    },
+   
+
 
 ]
