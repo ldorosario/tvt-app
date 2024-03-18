6f4fa2c226a92a86d0433b39a6cafd28393c03d8
Lucas do Rosário Pereira <ldorosario@users.noreply.github.com>
2024-03-17 21:00:51 -0300

    Update Membros.ts
    Adicionando Juliano Ferreira Machado De Leon

--- src/dados/Membros.ts
+++ src/dados/Membros.ts
@@ -1,10 +1,22 @@
 export const membrosEstaticos =
 [ 
     {
-    nome:"Nome Teste",
+    nome:"Juliano Ferreira Machado De Leon",
      dados:{
-      telefone:"000",
-      tipoSangue:"O-",
+      dataEntradaEquipe:"Sem info",
+      dataNascimento:"15/07/2004",
+      rg:"Sem info",
+      cpf:"86718371068",
+      telefone:"51 999212870",
+      email:"julianomachadodez@gmail.com",
+      endereco:"Dom Antônio cheuiche 79",
+      tipoSangue:"A+",
+      alergias:"Nega",
+      hospitalPreferencia:"Sem preferência",
+      contatosEmergencia: "51 9992702423 - Emanuela(Mãe)",
+      medicamentoUsoCont:"Nega",
+      problemaSaude: "Nega",
+      planoSaude: "IP"
     } 
      
     },
